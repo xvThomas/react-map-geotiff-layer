5c62727394fb274bc232c4dca502ef84a0d6a037
Xavier Thomas <xavier.thomas@cybeletech.com>
2023-11-02 09:38:41 +0100

    fix: remove useless tests

--- tests/geotiff.test.ts
+++
@@ -1,6 +0,0 @@
-import { getGeotiffData } from '../src/helpers/fetchGeotiff'
-
-describe('Reading Geotiff', () => {
-  it('renders without crashing', () => {})
-  getGeotiffData('file://000000.tif, 0')().then((res) => console.log(res))
-})
